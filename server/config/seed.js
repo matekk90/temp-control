3089b9b80a47f9a52fa1c6995ba5e477e6414681
Mateusz Krupop <MKp@axxiome.com>
2016-06-20 21:14:38 +0200

    Refactor initial server config/seed

--- server/config/seed.js
+++ server/config/seed.js
@@ -8,27 +8,32 @@ import Sensor from '../api/sensor/sensor.model';
 import User from '../api/user/user.model';
 
 Sensor.find({}).remove()
-  .then(() => {
-    Sensor.create({
-      name: 'ds18b20'
-    });
-  });
+    .then(function () {
+        Sensor.create({
+            name: 'Fermentation'
+        }, {
+            name: 'Kegerator'
+        });
+    })
+;
 
 User.find({}).remove()
-  .then(() => {
-    User.create({
-      provider: 'local',
-      name: 'Test User',
-      email: 'matekk90@gmail.com',
-      password: 'qwerty'
-    }, {
-      provider: 'local',
-      role: 'admin',
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: 'admin'
+    .then(function () {
+        User.create({
+            provider: 'local',
+            name: 'Test User',
+            email: 'matekk90@gmail.com',
+            password: 'qwerty'
+        }, {
+            provider: 'local',
+            role: 'admin',
+            name: 'Admin',
+            email: 'admin@example.com',
+            password: 'admin'
+        })
+            .then(function () {
+                console.log('finished populating users');
+            })
+        ;
     })
-    .then(() => {
-      console.log('finished populating users');
-    });
-  });
+;
